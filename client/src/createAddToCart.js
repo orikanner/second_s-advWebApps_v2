7206087b5914f85b4cb31e01320799177118617a
oriKanner <orikanner12345@gmail.com>
2022-12-12 15:03:25 +0200

    local storage cart

--- client/src/createAddToCart.js
+++ client/src/createAddToCart.js
@@ -1,18 +1,53 @@
 export const createAddToCart = (setItemCount, itemCount) => async (Product) => {
   // need to do something with this product
-  setItemCount(itemCount + 1);
+  // setItemCount(itemCount + 1);
   try {
-
-    const respone = await fetch("http://localhost:5000/cart/addToCart", {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify(Product),
-    });
-    if (respone.ok){}
-      //setItemCount(itemCount + 1);
-    // if (respone.ok) setItemCount((curr) => curr + 1);
-  } catch (err) {
+    const currCart = [];
+    if (!JSON.parse(localStorage.getItem("Cart")))
+      localStorage.setItem("Cart", currCart);
+    else{
+      JSON.parse(localStorage.getItem("Cart")).forEach(element => {
+        currCart.push(element);
+      });
+    }
+    currCart.push(Product);
+    localStorage.setItem("Cart",JSON.stringify(currCart));
+    setItemCount(currCart.length);
     
-    console.error("failed:", err.message);
-  }
-};
+  } catch(err){
+      console.error("failed To add to cart")
+  };
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// export const createAddToCart = (setItemCount, itemCount) => async (Product) => {
+//   // need to do something with this product
+//   // setItemCount(itemCount + 1);
+//   try {
+
+//     const response = await fetch("http://localhost:5000/cart/addToCart", {
+//       method: "POST",
+//       headers: { "Content-Type": "application/json" },
+//       body: JSON.stringify(Product),
+//     });
+//     if (response.ok){}
+//       setItemCount(itemCount + 1);
+//     // if (respone.ok) setItemCount((curr) => curr + 1);
+//   } catch (err) {
+    
+//     console.error("failed:", err.message);
+//   }
+// };
